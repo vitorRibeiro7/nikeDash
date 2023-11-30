4eed0967ed284eabdea55a42af95b7e916ea8dd0
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-11-30 10:31:37 -0300

    :rocket: sidebar improvements

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import NikeLogo from '../../../assets/logos/nike.svg';
 import { useCallback, useEffect, useState } from 'react';
 
@@ -49,56 +51,66 @@ const Sidebar = ({
 
   return (
     isVisible && (
-      <div
-        className={`sm:w-16 lg:w-64 bg-white h-full shadow-1xl overflow-clip min-h-screen z-50 transition-all duration-500 ${
-          isMobile && isVisible ? 'fixed w-8/12 top-0 left-0' : 'hidden'
-        }}`}
-      >
+      <>
         {isMobile && (
-          <div className="flex justify-end p-2">
-            <button
-              onClick={() => {
-                setDisableSidebar();
-              }}
-            >
-              <IoClose size="30" />
-            </button>
-          </div>
+          <div
+            className="fixed left-0 top-0 w-screen h-screen bg-black opacity-80"
+            onClick={() => {
+              setDisableSidebar();
+            }}
+          ></div>
         )}
-        <div className="flex items-center justify-center mt-2 w-fill h-32 p-1">
-          <img src={NikeLogo} alt="Nike Logo" className="w-24" />
-        </div>
-        <div className="">
-          <div className="mt-6">
-            {itens.map((item, index) => (
+        <div
+          className={`sm:w-16 lg:w-64 bg-white h-full shadow-1xl overflow-clip min-h-screen z-50 transition-all duration-500 ${
+            isMobile && isVisible ? 'fixed w-8/12 top-0 left-0' : 'hidden'
+          }}`}
+        >
+          {isMobile && (
+            <div className="flex justify-end p-2">
               <button
-                key={index}
-                className={`flex items-center pl-5 py-3 transition-all duration-500 hover:bg-gray-200 min-h-[52px] w-full border-l-4 ${
-                  active === item.name
-                    ? ' border-[#4AD697]'
-                    : 'border-transparent'
-                }`}
                 onClick={() => {
-                  handleActive(item);
-                  navigate(item.navTo);
-                  if (isMobile) {
-                    setDisableSidebar();
-                  }
+                  setDisableSidebar();
                 }}
               >
-                <span className={`text-[#151515]'`}>{item.icon} </span>
-                <h2
-                  className={`ml-2 text-[#151515] ${
-                    !isMobile && 'hidden'
-                  } lg:block text-xl whitespace-pre overflow-hidden transition-all duration-500`}
-                >
-                  {item.name}
-                </h2>
+                <IoClose size="30" />
               </button>
-            ))}
+            </div>
+          )}
+          <div className="flex items-center justify-center mt-2 w-fill h-32 p-1">
+            <img src={NikeLogo} alt="Nike Logo" className="w-24" />
+          </div>
+          <div className="">
+            <div className="mt-6">
+              {itens.map((item, index) => (
+                <button
+                  key={index}
+                  className={`flex items-center pl-5 py-3 transition-all duration-500 hover:bg-gray-200 min-h-[52px] w-full border-l-4 ${
+                    active === item.name
+                      ? ' border-[#4AD697]'
+                      : 'border-transparent'
+                  }`}
+                  onClick={() => {
+                    handleActive(item);
+                    navigate(item.navTo);
+                    if (isMobile) {
+                      setDisableSidebar();
+                    }
+                  }}
+                >
+                  <span className={`text-[#151515]'`}>{item.icon} </span>
+                  <h2
+                    className={`ml-2 text-[#151515] ${
+                      !isMobile && 'hidden'
+                    } lg:block text-xl whitespace-pre overflow-hidden transition-all duration-500`}
+                  >
+                    {item.name}
+                  </h2>
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </>
     )
   );
 };
